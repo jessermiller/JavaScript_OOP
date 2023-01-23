4c643d7411c5eed32ec8678eda6f258fc8e23c99
jessermiller <iamjaymiller@yahoo.com>
2023-01-23 01:26:50 -0600

    Finished exercises git push .

--- app.js
+++ app.js
@@ -39,17 +39,85 @@ class Coder extends Person {
     }
 }
 
-let coder = new Coder("James", 4, "Mississippi", ["walking", "driving"], "Web-Developer");
-coder.greeting();
-console.log(coder);
 
 
 console.log("EXERCISE 3:\n==========\n");
+let frank = new Person("Frank", 0, "New York", ["Tv", "Sports"]);
+let coder = new Coder("James", 4, "Mississippi", ["walking", "driving"], "Web-Developer");
+coder.greeting();
+coder.addHobby("reading");
+coder.removeHobby("driving");
+console.log(frank);
+console.log(coder);
 
-
-
-
-
-
-
+// I believe this is what I was instructed to do for exercise 3 
 console.log("EXERCISE 4:\n==========\n");
+
+class Calculator{
+    constructor(){
+        this.result = 0;
+    }
+
+    add(a,b) {
+        let res;
+        if(b === undefined) {
+            res = this.result + a;
+        } else {
+            res = a + b;
+        }
+        this.result = res;
+        return this.result;
+    }
+
+    subtract(a,b){
+        let res;
+        if(b === undefined){
+            res = this.result - a;
+        } else {
+            res = a - b;
+        }
+        this.result = res;
+        return this.result;
+    }
+
+    multiply(a,b) {
+        let res; 
+        if(b === undefined){
+            res = this.result * a;
+        } else {
+            res = a * b;
+        }
+        this.result = res;
+        return this.result;
+    }
+
+    divide(a,b){
+        let res;
+        if(b === undefined){
+            res = this.result / a;
+        } else {
+            res = a / b;
+        }
+        this.result = res;
+        return this.result;
+    }
+
+    display() {
+        console.log(this.result);
+    }
+}
+
+let calc = new Calculator();
+calc.add(1,1);
+calc.display();
+calc.subtract(8,4);
+calc.display();
+calc.multiply(3,2);
+calc.display();
+calc.divide(32,4);
+calc.display();
+calc.divide(32,4);
+calc.add(2);
+calc.multiply(2);
+calc.divide(5);
+calc.display();
